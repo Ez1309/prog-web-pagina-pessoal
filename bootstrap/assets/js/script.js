60cec61796a661ee288fe53b66b87ab194034091
Ezequiel Dominguez Santos <eze.dmgz06@gmail.com>
2026-09-17 12:26:00 -0300

    fix: ajustes no codigo bootstrap

--- bootstrap/assets/js/script.js
+++ bootstrap/assets/js/script.js
@@ -1,8 +1,6 @@
-// Seleção de elementos
 const root = document.documentElement;
 const themeBtn = document.getElementById("themeToggle");
 
-// Função para aplicar o tema e trocar o ícone
 function applyTheme(theme) {
     root.setAttribute("data-bs-theme", theme);
     localStorage.setItem("portfolio-theme", theme);
@@ -16,18 +14,15 @@ function applyTheme(theme) {
     }
 }
 
-// Inicialização: Verifica se já tem tema salvo ou busca preferência do Sistema Operacional
 const savedTheme = localStorage.getItem("portfolio-theme");
 
 if (savedTheme) {
     applyTheme(savedTheme);
 } else {
-    // Se não tem salvo, verifica se o sistema operacional está no modo escuro
     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
     applyTheme(prefersDark ? "dark" : "light");
 }
 
-// Evento de clique para o botão
 if (themeBtn) {
     themeBtn.addEventListener("click", () => {
         const currentTheme = root.getAttribute("data-bs-theme");
